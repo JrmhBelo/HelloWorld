3194ce4c098a4c8ce104fc1e806a83e925d9c31a
João Belo <jrmhbelo@gmail.com>
2020-11-06 13:18:20 +0000

    commit 3

--- helloworld/src/main/java/pt/iade/helloworld/controllers/GreeterController.java
+++ helloworld/src/main/java/pt/iade/helloworld/controllers/GreeterController.java
@@ -1,5 +1,4 @@
 package pt.iade.helloworld.controllers;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
@@ -10,12 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path="/api/greeter")
 public class GreeterController {
- private Logger logger = LoggerFactory.getLogger(GreeterController.class);
- @GetMapping(path = "", produces= MediaType.APPLICATION_JSON_VALUE)
- public String getGreeting() {
- logger.info("Saying Hello to the world");
- return "Hello World";
- }
+    private Logger logger = LoggerFactory.getLogger(GreeterController.class);
+    @GetMapping(path = "", produces= MediaType.APPLICATION_JSON_VALUE)
+    public String getGreeting() {
+        logger.info("Saying Hello to the world");
+        return "Hello World";
+    }
 
  @GetMapping(path = "{name}",
  produces= MediaType.APPLICATION_JSON_VALUE)
